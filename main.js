7da1027787a6d682903be78d63fcc46b50687f37
Ali Mohamed <alimohamed351@outlook.com>
2025-05-16 12:19:47 +0300

    added hero section

---
+++ main.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    let burger = document.querySelector('.sidebar');
+    burger.style.display = 'flex';
+}
+
+function hideSideBar() {
+    let hide = document.querySelector('.sidebar');
+    hide.style.display = 'none';
+}
