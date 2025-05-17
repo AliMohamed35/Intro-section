ec8d59621ad7f0282fcfbca31b545e94a4239df9
Ali Mohamed <alimohamed351@outlook.com>
2025-05-17 15:15:28 +0300

    configured sidebar

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function showSideBar() {
-    let burger = document.querySelector('.sidebar');
+    let burger = document.querySelector('.sideBar');
+    const body = document.body;
+    body.style.backgroundColor = '#00000094';
     burger.style.display = 'flex';
 }
 
 function hideSideBar() {
-    let hide = document.querySelector('.sidebar');
+    let hide = document.querySelector('.sideBar');
+    const body = document.body;
+    body.style.backgroundColor = 'white';
     hide.style.display = 'none';
 }
