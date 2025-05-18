7254c0d25b14722dbd06e341b8fd5aa9ffb6090f
Ali Mohamed <alimohamed351@outlook.com>
2025-05-18 17:39:43 +0300

    Added some responsivness enhansements

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 function showSideBar() {
     let burger = document.querySelector('.sideBar');
-    const body = document.body;
-    body.style.backgroundColor = '#00000094';
     burger.style.display = 'flex';
 }
 
 function hideSideBar() {
     let hide = document.querySelector('.sideBar');
-    const body = document.body;
-    body.style.backgroundColor = 'white';
     hide.style.display = 'none';
 }
